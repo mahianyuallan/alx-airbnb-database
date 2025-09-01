6364df89d84f33769b3301a5c7c1b85f07a5c1c3
Allan Mwenja <mwenjaallan18@gmail.com>
2025-09-01 18:09:44 +0300

    ROW_NUMBER() included

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -17,7 +17,7 @@ SELECT
     p.property_id,
     p.property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b 
     ON p.property_id = b.property_id
