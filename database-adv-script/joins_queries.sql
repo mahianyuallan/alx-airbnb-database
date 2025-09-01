362a892b8fe0e4cd83b2cd6f1aa683078a7b85f5
Allan Mwenja <mwenjaallan18@gmail.com>
2025-09-01 18:02:56 +0300

    Included ORDER BY in LEFT JOIN

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -21,7 +21,9 @@ SELECT
     r.comment
 FROM properties p
 LEFT JOIN reviews r 
-    ON p.property_id = r.property_id;
+    ON p.property_id = r.property_id
+ORDER BY p.property_id;
+
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
